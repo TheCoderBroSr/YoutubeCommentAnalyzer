2cbc87c70ce5435cf41a230213895bdf18d41eee
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-04-09 15:23:13 +0530

    server related stuff

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def processing():
     return render_template('index.html', emotion_perc=positivity_perc, error=err, video_response=video_response, comments=comments, note=note)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
