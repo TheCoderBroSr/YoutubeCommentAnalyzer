3ca078cdf95dd04485b8c43543a3dbfa1db19847
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-04-22 22:39:07 +0530

    typo

--- func.py
+++ func.py
@@ -2,7 +2,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import validators, re, pickle
 
-API_KEY = "YOUR KEY"
+API_KEY = "MY KEY"
 CLEANER = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 def load_file(filename):
